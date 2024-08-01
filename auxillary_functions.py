e12328ef34564ab1e18a42f7976b30be8a194b03
mendon-manoj <mj.ganiga@gmail.com>
2024-08-01 11:11:51 +0530

    Update auxillary_functions.py

--- auxillary_functions.py
+++ auxillary_functions.py
@@ -5,7 +5,7 @@ name = sys.argv[1]
 print ("Hello {}. How are you?".format(name))
 
 # command to to execute e.g. python test.py manoj
-
+#------------------------------------------------------------------------
 #code to read files from directory
 import os 
   
@@ -15,3 +15,24 @@ path = "C:/Users/C60091/Pictures"
 with os.scandir(path) as entries:
     for entry in entries:
         print(entry.name)
+
+#------------------------------------------------------------------------
+#code to slice video into images
+import cv2
+print('test')
+capture = cv2.VideoCapture('D:/drishti/KTSession1.mp4')
+print('test1')
+framenr = 0
+
+while (True):
+    success, frame = capture.read()
+    if success:
+        cv2.imwrite(f'D:/drishti/output/frame_{framenr}.jpg', frame)
+    else:
+        break
+    framenr += 1
+    print(framenr)    
+
+capture.release()
+
+#------------------------------------------------------------------------
